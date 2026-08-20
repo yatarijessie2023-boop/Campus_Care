03722242fef45ebebd65144494360f9a75f911b2
yatarijessie2023-boop <d1499111@o365.fcu.edu.tw>
2026-08-20 19:29:53 +0800

    Fix Leaflet marker icon on deployed site

--- frontend/src/components/ReportMap.vue
+++ frontend/src/components/ReportMap.vue
@@ -3,6 +3,16 @@ import { computed, onMounted, onBeforeUnmount, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { t } from '../i18n';
 import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
 
 const props = defineProps({
   latitude: [Number, String], longitude: [Number, String], editable: Boolean,
